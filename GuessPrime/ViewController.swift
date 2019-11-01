59e01acab4d3db9ef3d9ee3df5bb8941b1cf17a9
Mohammad Kiani <mohammadkiani@outlook.com>
2019-10-31 23:36:24 -0400

    Initial Commit

---
+++ GuessPrime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GuessPrime
+//
+//  Created by Mohammad Kiani on 2019-10-31.
+//  Copyright © 2019 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
